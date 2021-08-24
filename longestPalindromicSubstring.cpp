4e556492a095f9428460037d1b6ba3028da80f86
Prashant Kumar Pandey <prashantpandey041@gmail.com>
2021-08-24 19:29:19 +0530

    Update longestPalindromicSubstring.cpp

--- longestPalindromicSubstring.cpp
+++ longestPalindromicSubstring.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergedIntervals;
-        if(intervals.size() == 0) {
-            return mergedIntervals; 
-        }
-        sort(intervals.begin(), intervals.end());
-        vector<int> tempInterval = intervals[0]; 
+    string longestPalindrome(string s) {
+        if(s.length() <= 1) return s;
+        int max_len = 1;
+        int n = s.length();
+        int st = 0, end = 0;
         
-        for(auto it : intervals) {
-            if(it[0] <= tempInterval[1]) {
-                tempInterval[1] = max(it[1], tempInterval[1]); 
-            } else {
-                mergedIntervals.push_back(tempInterval); 
-                tempInterval = it; 
+        // Odd length
+        for(int i = 0; i < n-1; ++i){
+            int l = i, r = i;
+            while(l >= 0 && r < n){
+                if(s[l] == s[r]){
+                    l--; r++;
+                }else
+                    break;
+            }
+            int len = r-l-1;
+            if(len > max_len){
+                max_len = len;
+                st = l+1;
+                end = r-1;
             }
         }
-        mergedIntervals.push_back(tempInterval);
-        return mergedIntervals; 
+        
+        // Even length
+        for(int i = 0; i < n-1; ++i){
+            int l = i, r = i+1;
+            while(l >= 0 && r < n){
+                if(s[l] == s[r]){
+                    l--; r++;
+                }else
+                    break;
+            }
+            int len = r-l-1;
+            if(len > max_len){
+                max_len = len;
+                st = l+1;
+                end = r-1;
+            }
+        }
+        
+        return s.substr(st, max_len);
+        
     }
 };
